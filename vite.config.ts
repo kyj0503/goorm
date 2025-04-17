1987c3e5964e1c248158893a6887612c11fb18f5
kyj0503 <heroria0503@gmail.com>
2025-04-17 22:08:59 +0900

    중복 메시지 문제 해결, 로컬 서버와 채팅 잘 됨.

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,20 @@ export default defineConfig({
   plugins: [react()],
   define: {
     global: 'window'
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // 백엔드 API 서버 주소
+        changeOrigin: true,
+        secure: false
+      },
+      '/ws': {
+        target: 'http://localhost:8080', // 백엔드 WebSocket 서버 주소
+        changeOrigin: true,
+        secure: false,
+        ws: true
+      }
+    }
   }
 })
